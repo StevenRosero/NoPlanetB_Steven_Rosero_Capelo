e00ec51276aae9b1b9847bb91bd02f070d1a9583
steve <steven.ros.cap@gmail.com>
2019-03-08 10:56:10 +0100

    Examen

---
+++ app/src/androidTest/java/com/example/noplanetb_steven_rosero_capelo/ExampleInstrumentedTest.java
@@ -0,0 +1,65 @@
+package com.example.noplanetb_steven_rosero_capelo;
+/*
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+*/
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ *//*
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest extends ActivityInstrumentationTestCase2<BLogIn> {
+
+    private Button loginbtn;
+    private EditText email;
+    private EditText passw;
+
+    public ExampleInstrumentedTest() {
+        super(BLogIn.class);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        BLogIn actividad = getActivity();
+        email = actividad.findViewById(R.id.etuser);
+        passw = actividad.findViewById(R.id.etpwd);
+        loginbtn = actividad.findViewById(R.id.btnLogIn);
+
+    }
+
+//    protected void tearDown() throws Exception {
+//        super.tearDown();
+//    }
+
+    private static final String USERNAME = "steven";
+    private static final String PASSWORD = "123456";
+
+    public void testLoginSignup() {
+//        onView(withId(R.id.email)).perform(typeText(USERNAME));
+//        onView(withId(R.id.password)).perform(typeText(PASSWORD));
+
+        // first tap on value1 entry
+        TouchUtils.tapView(this, email);
+        getInstrumentation().sendStringSync(USERNAME);
+        // now on value2 entry
+        TouchUtils.tapView(this, passw);
+        getInstrumentation().sendStringSync(PASSWORD);
+        // now on login button
+        TouchUtils.tapView(this, loginbtn);
+
+    }
+}*/
